107531c9e986c921620ad28a794defb0245e2da0
Jyoti Patel <jyotiinfinite72@gmail.com>
2025-08-24 14:59:54 +0530

    hydrodynamics

--- optimize-gifs.sh
+++ optimize-gifs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SRC_DIR="./aether"  # Root folder to start from
-DEST_DIR="./optimized_gifs/aether"
+SRC_DIR="./input_gifs"  # Root folder to start from
+DEST_DIR="./optimized_gifs"
 MAX_DIM=1200
 
 echo "Optimizing GIFs from $SRC_DIR to $DEST_DIR ..."
